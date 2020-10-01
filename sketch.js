f2b3833f450c97b206a863112b0f7838d69f83cb
Jedd Panopio <37092739+JeddPanopio@users.noreply.github.com>
2020-10-01 00:36:13 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,21 +13,34 @@ let pokemon = [{
 }];
 
 
-let randomIndex;
+
 function setup() {
   createCanvas(600, 600);
   background(200);
+	textSize(32);
+	text("click to randomize",50,50);
 
 
 
 
 
 
-randomIndex = int(random(pokemon.length));
-text(pokemon[randomIndex].name,50,50 );
 
 }
+function mousePressed(){
+	background(random(200,255));
+		if (pokemon[0]) {
 
+
+			randomIndex = int(random(pokemon.length));
+			text(pokemon[randomIndex].name,50,50 );
+			pokemon.splice(randomIndex, 1);
+
+	}else {
+		background(200,255);
+		text("A WILD POKEMON RAN AWAY",50, 50);
+	}
+}
 function draw() {
 
 }
