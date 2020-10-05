1ba9574b55660a226495372af45c67664518e4d7
Jedd Panopio <37092739+JeddPanopio@users.noreply.github.com>
2020-10-05 01:41:17 -0700

    added button and randomized

--- sketch.js
+++ sketch.js
@@ -7,56 +7,75 @@ let pokemon = [{
 }, {
   name: "Blastoise",
   type: "Water"
-} ,{
+}, {
   name: "Venusaur",
   type: "Grass"
 }];
 
 let randomIndex;
 let animating = false;
+let pkmn = [];
+let imageCount = 0;
+let button;
+
+function preload() {
+  for (let i = 0; i <= 3; i++) {
+    pkmn[i] = loadImage("images/pkmn_" + i + ".JPG");
+  }
+}
 
 function setup() {
   createCanvas(600, 600);
   background(200);
-	textSize(32);
-	text("ZA WARUDO",50,50);
-	setTimeout(changeBackground, 1000);
+  textSize(32);
+  text("ZA WARUDO", 50, 50);
+  setTimeout(changeBackground, 1000);
+  imageMode(CENTER);
+  frameRate(9);
+  button =  createButton("click to randomize");
+  button.mousePressed(buttonPressed);
 }
 
-function changeBackground(){
-	if (counter <= 5) {
-		counter++;
-		console.log(counter)
-		background(random(255),random(255),random(255));
-		setTimeout(changeBackground, 1000);
-	}else {
+function changeBackground() {
+  if (counter <= 5) {
+    counter++;
+    console.log(counter)
+    background(random(255), random(255), random(255));
+    setTimeout(changeBackground, 1000);
+  } else {
 
-	}
-}
-function randomizer(){
-  animating = false;
-
-  if (pokemon[0]) {
-    background(random(200,255));
-    randomIndex = int(random(pokemon.length));
-    text(pokemon[randomIndex].name,50,50 );
-    pokemon.splice(randomIndex, 1);
-
-}else {
-  background(200,255);
-  text("MUDA MUDA MUDA MUDA MUDA",50, 50);
   }
 }
 
-function mousePressed(){
+function randomizer() {
+  animating = false;
+
+  if (pokemon[0]) {
+    background(random(200, 255));
+    randomIndex = int(random(pokemon.length));
+    text(pokemon[randomIndex].name, 50, 50);
+    pokemon.splice(randomIndex, 1);
+
+  } else {
+    background(200, 255);
+    text("MUDA MUDA MUDA MUDA MUDA", 50, 50);
+  }
+}
+
+function buttonPressed() {
   animating = true;
   setTimeout(randomizer, 2000);
 
 }
+
 function draw() {
   if (animating == true) {
 
-    ellipse(random(width), random(height), random(50,200));
-    color(HSB);
+    image(pkmn[imageCount], width / 2, height / 2);
+    if (imageCount < pkmn.length - 1) {
+      imageCount++;
+    } else {
+      imageCount = 0;
+    }
   }
 }
